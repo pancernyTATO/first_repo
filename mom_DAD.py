801824b827fe38f508144da761a9826f123a7eac
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 13:05:40 +0100

    mom_DAD

---
+++ mom_DAD.py
@@ -0,0 +1,10 @@
+my_string1 = 'mom'
+mystring2 = 'dad'
+sum1 = my_string1 + mystring2
+print(sum1)
+
+sum2 = 'my ' + my_string1 + ' and my ' + mystring2
+print(sum2)
+
+print(sum2[3:6])
+print(sum2[-4:-1])
