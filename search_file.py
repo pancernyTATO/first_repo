f936323e6fc0e96145761c1301d97b3fd2596386
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 12:45:29 +0100

    search_file

---
+++ search_file.py
@@ -0,0 +1,6 @@
+import os
+import time
+
+os.system('cmd /c "cd C://Users//vdi-student//Desktop && dir /s new.txt >> result.txt"')
+# os.system('cmd c "cd C:\\Users\\vdi-student\\Desktop"')
+# os.system('cmd c "dir /s new.txt >> result.txt"')
