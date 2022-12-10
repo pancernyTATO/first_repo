66769576961f746768c0216433db014ca5227093
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 13:30:42 +0100

    nazwaczas

---
+++ NAZWACZAS.py
@@ -0,0 +1,8 @@
+import datetime
+import time
+name = 'raport.txt'
+for i in range(10):
+    now = datetime.datetime.now()
+    my_now = now.strftime('%H%M%S')
+    print(name[:5] + my_now + name[-4:])
+    time.sleep(2)
