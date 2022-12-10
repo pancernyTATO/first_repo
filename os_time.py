e298bcda79cc0c934b19847f2a0a9fe346bf731a
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 11:50:10 +0100

    program_folder

---
+++ os_time.py
@@ -0,0 +1,12 @@
+import os
+import time
+print('a')
+print(os.getcwd())
+
+os.mkdir('nowy_folder')
+
+time.sleep(5)
+
+os.rename('nowy_folder', 'nowy_folder_2')
+time.sleep(5)
+qqos.rmdir('nowy_folder_2')
