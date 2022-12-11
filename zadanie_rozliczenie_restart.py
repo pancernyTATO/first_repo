efa7d48e0f7021dc4c82c47d92e1b6cfde04fd94
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 09:32:01 +0100

    zadanie_rozliczenie_restart

---
+++ zadanie_rozliczenie_restart.py
@@ -0,0 +1,21 @@
+#1. Odczyt pliku csv z zapisem poszczegolnych pol
+path = 'C:\\Users\\vdi-student\\Desktop\\rozliczenie.csv'
+mode = 'r'
+with open (path, mode) as plik2:
+    content = plik2.readlines()
+#print(content[3]) #wyswietlenie linii 4
+for i in range (len(content)):
+    #print(content[i])
+    content[i] = content[i].split(',')
+#     print(content[i])
+# print(content[2][2])
+
+#2.Obliczenie sredniej wyplaty
+total = 0
+for i in range(1, len(content)):
+    total = total + int(content[i][1])
+print(total)
+average = total / (len(content)-1)
+print(round(average,2))
+
+#3. Obliczanie liczby kobiet na macierzynskim
